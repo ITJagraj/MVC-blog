fe512ed9acec4cf31651f4e4b7fd16c2e0b3acad
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-07-26 22:43:35 -0400

    deploying on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,13 @@
-const Sequelize = require('sequelize');
-
-
-require('dotenv').config();
-
-let sequelize;
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize('mvc_blog_db','root', 'Itprofessional@31', {
-        host: 'localhost',
-        dialect: 'mysql',
+const Sequelize = require("sequelize");
+require("dotenv").config();
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+            decimalNumbers: true,
+        },
         port: 3306
     });
-}
-
 module.exports = sequelize;
